bc021697fe5b1ba127d5bdac619d436646c8ccd3
Kartik Bhalla <kartik.bhalla@upgrad.com>
2024-05-28 14:19:39 +0530

    footer: add component and get values from cms
    Signed-off-by: Kartik Bhalla <kartik.bhalla@upgrad.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Header from '@components/header';
-// import Footer from '@components/footer';
 import Banner from '@components/banner';
 import Process from '@components/process';
 import Testimonials from '@components/testimonials';
@@ -9,11 +8,12 @@ import Printing from '@components/printing';
 import PriceEstimator from '@components/priceEstimator';
 import Gallery from '@components/gallery';
 import Cardboard from '@components/cardboard';
+import About from '@components/about';
+import Footer from '@components/footer';
 
 import { combineKeyValuePairs } from '@utils/sheets';
 
 import styles from './page.module.scss';
-import About from '@components/about';
 
 export default async function Home(props) {
 	const {
@@ -28,190 +28,203 @@ export default async function Home(props) {
 		gallery,
 		cardboardTypes,
 		aboutUs,
+    footer
 	} = await getContent();
 
   const isPriceEstimatorVisible = priceEstimator.visible.toLowerCase() === 'true';
 
 	return (
-		<main className={styles.main}>
-			<Header
-				button={{
-					label: header.button_label,
-					link: header.button_link,
-				}}
-				links={combineKeyValuePairs({
-					data: header,
-					max: 6,
-					key: 'nav',
-					types: ['label', 'link'],
-				})}
-				logo={{
-					id: header.logo_id,
-					alt: header.logo_alt,
-				}}
-			/>
-			<Banner
-				heading={banner.heading}
-				description={banner.description}
-				buttons={combineKeyValuePairs({
-					data: banner,
-					max: 2,
-					key: 'button',
-					types: ['label', 'link'],
-				})}
-			/>
+    <main className={styles.main}>
+      <Header
+        button={{
+          label: header.button_label,
+          link: header.button_link,
+        }}
+        links={combineKeyValuePairs({
+          data: header,
+          max: 6,
+          key: "nav",
+          types: ["label", "link"],
+        })}
+        logo={{
+          id: header.logo_id,
+          alt: header.logo_alt,
+        }}
+      />
+      <Banner
+        heading={banner.heading}
+        description={banner.description}
+        buttons={combineKeyValuePairs({
+          data: banner,
+          max: 2,
+          key: "button",
+          types: ["label", "link"],
+        })}
+      />
 
+      <About
+        heading={aboutUs.heading}
+        description={aboutUs.description}
+        banner={aboutUs.banner}
+        stats={combineKeyValuePairs({
+          data: aboutUs,
+          max: 3,
+          key: "stats",
+          types: ["title", "description", "count"],
+        })}
+      />
+      <Gallery
+        heading={gallery.heading}
+        photos={combineKeyValuePairs({
+          data: gallery,
+          max: 12,
+          key: "image",
+          types: ["id", "label", "orientation"],
+        })}
+      />
+      <Process
+        heading={process.heading}
+        steps={combineKeyValuePairs({
+          data: process,
+          max: 3,
+          key: "step",
+          types: ["label", "description"],
+        })}
+      />
 
-			<About
-				heading={aboutUs.heading}
-				description={aboutUs.description}
-				banner={aboutUs.banner}
-				stats={combineKeyValuePairs({
-					data: aboutUs,
-					max: 3,
-					key: 'stats',
-					types: ['title', 'description', 'count'],
-				})}
-			/>
-			<Gallery
-				heading={gallery.heading}
-				photos={combineKeyValuePairs({
-					data: gallery,
-					max: 12,
-					key: 'image',
-					types: ['id', 'label', 'orientation'],
-				})}
-			/>
-			<Process
-				heading={process.heading}
-				steps={combineKeyValuePairs({
-					data: process,
-					max: 3,
-					key: 'step',
-					types: ['label', 'description'],
-				})}
-			/>
-
-			<Cardboard
-				types={combineKeyValuePairs({
-					data: cardboardTypes,
-					max: 8,
-					key: 'cardboard',
-					types: ['id', 'label', 'description'],
-				})}
-				heading={cardboardTypes.heading}
-				description={cardboardTypes.description}
-			/>
-			<Testimonials
-				heading={testimonials.heading}
-				testimonials={combineKeyValuePairs({
-					data: testimonials,
-					max: 6,
-					key: 'testimonial',
-					types: ['message', 'name', 'client'],
-				})}
+      <Cardboard
+        types={combineKeyValuePairs({
+          data: cardboardTypes,
+          max: 8,
+          key: "cardboard",
+          types: ["id", "label", "description"],
+        })}
+        heading={cardboardTypes.heading}
+        description={cardboardTypes.description}
+      />
+      <Testimonials
+        heading={testimonials.heading}
+        testimonials={combineKeyValuePairs({
+          data: testimonials,
+          max: 6,
+          key: "testimonial",
+          types: ["message", "name", "client"],
+        })}
         isPriceEstimatorVisible={isPriceEstimatorVisible}
-			/>
+      />
 
-			<PriceEstimator
-				visible={isPriceEstimatorVisible}
-				heading={priceEstimator.heading}
-				description={priceEstimator.description}
-				disclaimer={priceEstimator.disclaimer}
-				fields={{
-					material: {
-						title: priceEstimator.material_title,
-						values: combineKeyValuePairs({
-							data: priceEstimator,
-							max: 5,
-							key: 'material',
-							types: ['label', 'code'],
-						}),
-					},
-					cardboard: {
-						title: priceEstimator.cardboard_title,
-						values: combineKeyValuePairs({
-							data: priceEstimator,
-							max: 8,
-							key: 'cardboard',
-							types: ['label', 'materials', 'code'],
-						}),
-					},
-					printing: {
-						title: priceEstimator.printing_title,
-						values: combineKeyValuePairs({
-							data: priceEstimator,
-							max: 8,
-							key: 'printing',
-							types: ['label', 'materials', 'code'],
-						}),
-					},
-					lamination: {
-						title: priceEstimator.lamination_title,
-						values: combineKeyValuePairs({
-							data: priceEstimator,
-							max: 5,
-							key: 'lamination',
-							types: ['label', 'materials', 'printing', 'code'],
-						}),
-					},
-					dimensions: {
-						title: priceEstimator.dimensions_title,
-						height: priceEstimator.dimensions_height_title,
-						width: priceEstimator.dimensions_width_title,
-						length: priceEstimator.dimensions_length_title,
-					},
-					quantity: {
-						title: priceEstimator.quantity_title,
-						min: priceEstimator.quantity_minimum_value,
-					},
-				}}
-			/>
-			<Clients
-				heading={clients.heading}
-				description={clients.description}
-				clients={combineKeyValuePairs({
-					data: clients,
-					max: 8,
-					key: 'client',
-					types: ['id'],
-				}).map(({ id }) => id)}
-			/>
+      <PriceEstimator
+        visible={isPriceEstimatorVisible}
+        heading={priceEstimator.heading}
+        description={priceEstimator.description}
+        disclaimer={priceEstimator.disclaimer}
+        fields={{
+          material: {
+            title: priceEstimator.material_title,
+            values: combineKeyValuePairs({
+              data: priceEstimator,
+              max: 5,
+              key: "material",
+              types: ["label", "code"],
+            }),
+          },
+          cardboard: {
+            title: priceEstimator.cardboard_title,
+            values: combineKeyValuePairs({
+              data: priceEstimator,
+              max: 8,
+              key: "cardboard",
+              types: ["label", "materials", "code"],
+            }),
+          },
+          printing: {
+            title: priceEstimator.printing_title,
+            values: combineKeyValuePairs({
+              data: priceEstimator,
+              max: 8,
+              key: "printing",
+              types: ["label", "materials", "code"],
+            }),
+          },
+          lamination: {
+            title: priceEstimator.lamination_title,
+            values: combineKeyValuePairs({
+              data: priceEstimator,
+              max: 5,
+              key: "lamination",
+              types: ["label", "materials", "printing", "code"],
+            }),
+          },
+          dimensions: {
+            title: priceEstimator.dimensions_title,
+            height: priceEstimator.dimensions_height_title,
+            width: priceEstimator.dimensions_width_title,
+            length: priceEstimator.dimensions_length_title,
+          },
+          quantity: {
+            title: priceEstimator.quantity_title,
+            min: priceEstimator.quantity_minimum_value,
+          },
+        }}
+      />
+      <Clients
+        heading={clients.heading}
+        description={clients.description}
+        clients={combineKeyValuePairs({
+          data: clients,
+          max: 8,
+          key: "client",
+          types: ["id"],
+        }).map(({ id }) => id)}
+      />
 
-			<Printing
-				heading={printingTypes.heading}
-				description={printingTypes.description}
-				types={combineKeyValuePairs({
-					data: printingTypes,
-					max: 8,
-					key: 'printing',
-					types: ['id', 'label'],
-				})}
-			/>
-			<ContactUs
-				heading={contactUs.heading}
-				description={contactUs.description}
-				address={{
-					label: contactUs.address_label,
-					link: contactUs.address_link,
-				}}
-				email={{ label: contactUs.email_label, value: contactUs.email_value }}
-				phone={{ label: contactUs.phone_label, value: contactUs.phone_value }}
-				form={{
-					heading: contactUs.form_heading,
-					name: contactUs.form_name_label,
-					phone: contactUs.form_phone_label,
-					email: contactUs.form_email_label,
-					button: contactUs.form_button_label,
-					message: contactUs.form_message_label,
-					validationMessages: {
-						success: contactUs.form_success_label,
-						error: contactUs.form_error_label,
-					},
-				}}
-			/>
-		</main>
-	);
+      <Printing
+        heading={printingTypes.heading}
+        description={printingTypes.description}
+        types={combineKeyValuePairs({
+          data: printingTypes,
+          max: 8,
+          key: "printing",
+          types: ["id", "label"],
+        })}
+      />
+      <ContactUs
+        heading={contactUs.heading}
+        description={contactUs.description}
+        address={{
+          label: contactUs.address_label,
+          link: contactUs.address_link,
+        }}
+        email={{ label: contactUs.email_label, value: contactUs.email_value }}
+        phone={{ label: contactUs.phone_label, value: contactUs.phone_value }}
+        form={{
+          heading: contactUs.form_heading,
+          name: contactUs.form_name_label,
+          phone: contactUs.form_phone_label,
+          email: contactUs.form_email_label,
+          button: contactUs.form_button_label,
+          message: contactUs.form_message_label,
+          validationMessages: {
+            success: contactUs.form_success_label,
+            error: contactUs.form_error_label,
+          },
+        }}
+      />
+      <Footer
+        links={{
+          twitter: footer.twitter,
+          instagram: footer.instagram,
+          facebook: footer.facebook,
+        }}
+        description={footer.description}
+        copyright={footer.copyright}
+        logo={{
+          id: footer.logo_id,
+          alt: footer.logo_alt,
+        }}
+      />
+    </main>
+  );
 }
 
 async function getContent() {
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,142 +1,56 @@
-'use client';
+"use client";
 
-// import cn from 'classnames';
+import Image from "next/image";
 
-// import { Col, Container, Navbar, Row } from 'react-bootstrap';
-import Image from 'next/image';
+import { getGoogleImageUrl } from "@utils/drive";
 
-import Facebook from '@icons/facebook';
-import Instagram from '@icons/instagram';
-import Twitter from '@icons/twitter';
+import Facebook from "@icons/facebook";
+import Instagram from "@icons/instagram";
+import Twitter from "@icons/twitter";
 
-const Footer = () => {
-	return (
-		<footer className='' style={{ backgroundColor: '#edf4e4' }} id='footer'>
-			<div className='pb-3'>
-				<div className='row-cols-1  row-cols-md-5 py-5 mb-3 border-bottom '>
-					<div className='d-flex flex-column mb-5 col-md-6 align-items-center  align-items-md-start'>
-						{/* <Navbar.Brand href={'headerContent.logo.href'}>
-							<Image
-								priority
-								src={'/logo.webp'}
-								className='mb-3'
-								width={200}
-								height={84}
-								alt={'headerContent.logo.alt'}
-								quality={100}
-							/>
-						</Navbar.Brand> */}
-						<p style={{ maxWidth: 200 }} className='text-center text-md-start'>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-							sunt.
-						</p>
-						<div style={{ display: 'flex', gap: 20 }}>
-							<Twitter style={{ height: 20, strokeWidth: 2 }} />
-							<Instagram style={{ height: 20, strokeWidth: 2 }} />
-							<Facebook style={{ height: 20, strokeWidth: 2 }} />
-						</div>
-					</div>
+import styles from "@components/footer/index.module.scss";
 
-					{/* <Col className="mb-3"></Col> */}
+const Footer = ({ links, description, copyright, logo }) => {
+  const socials = [
+    {
+      Component: Twitter,
+      link: links.twitter,
+    },
+    {
+      Component: Facebook,
+      link: links.facebook,
+    },
+    {
+      Component: Instagram,
+      link: links.instagram,
+    },
+  ];
 
-					<div className='mb-3 col-md-2 d-flex align-items-center flex-column align-items-md-start'>
-						<h5>Section</h5>
-						<ul className='nav flex-column text-center text-md-start'>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Home
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Features
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Pricing
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									FAQs
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									About
-								</a>
-							</li>
-						</ul>
-					</div>
+  return (
+    <footer id="footer" className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Image
+          src={getGoogleImageUrl(logo.id)}
+          alt={logo.alt}
+          fill
+          priority
+          quality={100}
+        />
+      </div>
 
-					<div className='mb-3 col-md-2 d-flex align-items-center flex-column align-items-md-start'>
-						<h5>Section</h5>
-						<ul className='nav flex-column text-center text-md-start'>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Home
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Features
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Pricing
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									FAQs
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									About
-								</a>
-							</li>
-						</ul>
-					</div>
+      <p className={styles.description}>{description}</p>
 
-					<div className='mb-3 col-md-2 d-flex align-items-center flex-column align-items-md-start'>
-						<h5>Section</h5>
-						<ul className='nav flex-column text-center text-md-start'>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Home
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Features
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									Pricing
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									FAQs
-								</a>
-							</li>
-							<li className='nav-item mb-2'>
-								<a href='#' className='nav-link p-0 text-muted'>
-									About
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
+      <div className={styles.socials}>
+        {socials.map((s, i) => (
+          <a href={s.link} key={i}>
+            <s.Component className={styles.socialIcon} />
+          </a>
+        ))}
+      </div>
 
-				<p className='text-center text-muted'>© 2023 Diamond Printers, Inc</p>
-			</div>
-		</footer>
-	);
+      <div className={styles.copyright}>{copyright}</div>
+    </footer>
+  );
 };
 
 export default Footer;
