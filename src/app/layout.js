449167ff72938b1f0ee2225f95310367bd2b0710
Kartik Bhalla <kartik.bhalla@upgrad.com>
2025-05-10 23:59:30 +0530

    src: add meta image and logo
    Signed-off-by: Kartik Bhalla <kartik.bhalla@upgrad.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,36 +2,80 @@ import './globals.scss';
 
 import { lato, leagueSpartan } from '@utils/fonts';
 
+const title = 'Diamond Printers';
+const description =
+  'Diamond Printers - Offset, Mini Offset Screen Printers, Computer Stationary, Design, Sticker & Banners';
+const keywords = [
+  title,
+  'Diamond',
+  'Printers',
+  'Cardboard',
+  'Printing',
+  'Stickers',
+  'Banners',
+  'Boxes',
+  'Labels',
+  'Stationary',
+  'Stationery',
+  'UV',
+  'Hybrid',
+  'Foil',
+  'Rainbow Foil',
+  'PLY',
+  'Rama Road',
+  'Raghav',
+  'Guatam',
+  'Vineet',
+  'Sachdeva',
+];
+const url = 'https://www.diamondprinters.in';
+const metaImage = `${url}/diamond-meta.webp`;
+const logo = `${url}/logo.png`;
+
 export const metadata = {
-  title: 'Diamond Printers',
-  description:
-    'Diamond Printers - Offset, Mini Offset Screen Printers, Computer Stationary, Design, Sticker & Banners,',
-  keywords: [
-    'Diamond Printers',
-    'Diamond',
-    'Printers',
-    'Cardboard',
-    'Printing',
-    'Stickers',
-    'Banners',
-    'Boxes',
-    'Labels',
-    'Stationary',
-    'Stationery',
-    'UV',
-    'Hybrid',
-    'Foil',
-    'Rainbow Foil',
-    'PLY',
-    'Rama Road',
-    'Raghav',
-    'Guatam',
-    'Vineet',
-    'Sachdeva',
-  ],
+  title,
+  description,
+  keywords,
   referrer: 'origin-when-cross-origin',
   authors: [{ name: 'Gautam Sachdeva' }],
   creator: 'Kartik Bhalla',
+
+  openGraph: {
+    title,
+    description,
+    url,
+    images: [
+      {
+        url: metaImage,
+        width: 1920,
+        height: 1080,
+        alt: title,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: [
+      {
+        url: `${url}/diamond-printers-meta.webp`,
+        alt: title,
+      },
+    ],
+    site: `@${title}`,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: {
+      type: 'image/png',
+      url: logo,
+    },
+  },
+  referrer: 'origin-when-cross-origin',
 };
 
 export default function RootLayout({ children }) {
